361213c20bfe5669ac9da9d79842e8329c24f303
Rose <ryu@wish.com>
2021-11-10 17:47:03 +0800

    f input show

--- components/input/doc/show.vue
+++ components/input/doc/show.vue
@@ -1,27 +1,93 @@
 <template>
   <div>
     <div class="demo-input-group">
-      <wt-input label="姓名" value="222" @input="input">
+      <wt-input label="联系人" placeholder="请输入">
         <wt-icon name="user" slot="prefix" />
+      </wt-input>
+      <wt-input label="联系人" disabled placeholder="请输入">
+        <wt-icon name="user" slot="prefix" />
+      </wt-input>
+      <wt-input label="联系人" invalid validation="请输入正确的内容" placeholder="请输入">
+        <wt-icon name="user" slot="prefix" />
+      </wt-input>
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input placeholder="请输入" />
+      <wt-input disabled placeholder="请输入" />
+      <wt-input invalid validation="请输入正确的内容" placeholder="请输入" />
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input placeholder="请输入">
+        <span slot="prefix">¥</span>
+      </wt-input>
+      <wt-input disabled placeholder="请输入">
+        <span slot="prefix">¥</span>
+      </wt-input>
+      <wt-input invalid validation="请输入正确的内容" placeholder="请输入">
+        <span slot="prefix">¥</span>
+      </wt-input>
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input placeholder="请输入">
         <span slot="suffix">RMB</span>
       </wt-input>
-      <wt-input label="姓名" value="222" @input="input" invalid>
-        <wt-icon name="eye" slot="suffix" />
+      <wt-input disabled placeholder="请输入">
+        <span slot="suffix">RMB</span>
       </wt-input>
-      <wt-input label="姓名" value="222" @input="input" disabled />
-      <wt-input full-width placeholder="请输入姓名" />
-      <wt-input type="line" label="姓名" value="222" @input="input">
-        <span slot="prefix">RMB</span>
-        <wt-icon name="eye" slot="suffix" />
+      <wt-input invalid validation="请输入正确的内容" placeholder="请输入">
+        <span slot="suffix">RMB</span>
       </wt-input>
-      <wt-input type="line" label="姓名" value="222" @input="input" invalid validation="请输入正确的姓名" />
-      <wt-input type="line" label="姓名" value="222" @input="input" disabled />
-      <wt-input type="line" full-width placeholder="请输入姓名" />
     </div>
+
     <div class="demo-input-group">
-      <wt-input label="多行输入标题" type="textarea" placeholder="请输入姓名" autosize />
-      <wt-input label="多行输入标题" type="textarea" disabled placeholder="请输入姓名" />
-      <wt-input type="textarea" placeholder="请输入姓名" invalid validation="请输入正确的姓名" />
+      <wt-input type="line" label="联系人" placeholder="请输入">
+        <wt-icon name="user" slot="prefix" />
+      </wt-input>
+      <wt-input type="line" label="联系人" disabled placeholder="请输入">
+        <wt-icon name="user" slot="prefix" />
+      </wt-input>
+      <wt-input type="line" label="联系人" invalid validation="请输入正确的内容" placeholder="请输入">
+        <wt-icon name="user" slot="prefix" />
+      </wt-input>
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input type="line" placeholder="请输入" />
+      <wt-input type="line" disabled placeholder="请输入" />
+      <wt-input type="line" invalid validation="请输入正确的内容" placeholder="请输入" />
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input type="line" placeholder="请输入">
+        <span slot="prefix">¥</span>
+      </wt-input>
+      <wt-input type="line" disabled placeholder="请输入">
+        <span slot="prefix">¥</span>
+      </wt-input>
+      <wt-input type="line" invalid validation="请输入正确的内容" placeholder="请输入">
+        <span slot="prefix">¥</span>
+      </wt-input>
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input type="line" placeholder="请输入">
+        <span slot="suffix">RMB</span>
+      </wt-input>
+      <wt-input type="line" disabled placeholder="请输入">
+        <span slot="suffix">RMB</span>
+      </wt-input>
+      <wt-input type="line" invalid validation="请输入正确的内容" placeholder="请输入">
+        <span slot="suffix">RMB</span>
+      </wt-input>
+    </div>
+
+    <div class="demo-input-group">
+      <wt-input type="textarea" placeholder="请输入" autosize />
+      <wt-input type="textarea" disabled placeholder="请输入" autosize />
+      <wt-input type="textarea" invalid placeholder="请输入" validation="请输入正确的内容" autosize />
     </div>
   </div>
 </template>
