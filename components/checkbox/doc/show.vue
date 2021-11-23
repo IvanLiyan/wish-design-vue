6bfe7624654534c82ae0e8a8d6508b427c671a45
Rose <ryu@wish.com>
2021-11-23 16:42:36 +0800

    m

--- components/checkbox/doc/show.vue
+++ components/checkbox/doc/show.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <h6>Checkbox-group disabled</h6>
+    <div class="demo-container">
+      <wt-checkbox-group v-model="groupValue" disabled>
+        <wt-checkbox value="1">选项</wt-checkbox>
+        <wt-checkbox value="2">选项</wt-checkbox>
+        <wt-checkbox value="3">选项</wt-checkbox>
+      </wt-checkbox-group>
+    </div>
     <h6>单选框组事件反馈，请打开控制台查看</h6>
     <div class="demo-container">
       <wt-checkbox v-model="checkedValue" @input="handleCheck">项目内容</wt-checkbox>
