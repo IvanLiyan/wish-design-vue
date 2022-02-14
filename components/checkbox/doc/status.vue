cf9b7104640a8560de376ca7c87ed6ed2079f5b0
Rose <ryu@wish.com>
2022-02-14 15:25:31 +0800

    checkbox demo display

--- components/checkbox/doc/status.vue
+++ components/checkbox/doc/status.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="demo-container">
-    <wt-checkbox v-model="checked1">item content<span slot="hint">desc should be short</span></wt-checkbox>
-    <wt-checkbox v-model="checked2">item content<span slot="hint">desc should be short</span></wt-checkbox>
-    <wt-checkbox disabled checked>item content<span slot="hint">desc should be short</span></wt-checkbox>
-    <wt-checkbox disabled>item content<span slot="hint">desc should be short</span></wt-checkbox>
+  <div>
+    <div class="demo-container">
+      <wt-checkbox v-model="checked1">item content<span slot="hint">desc should be short</span></wt-checkbox>
+      <wt-checkbox v-model="checked2">item content<span slot="hint">desc should be short</span></wt-checkbox>
+    </div>
+    <div class="demo-container">
+      <wt-checkbox disabled checked>item content<span slot="hint">desc should be short</span></wt-checkbox>
+      <wt-checkbox disabled>item content<span slot="hint">desc should be short</span></wt-checkbox>
+    </div>
   </div>
 </template>
 <script>
