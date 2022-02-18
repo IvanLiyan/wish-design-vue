47d42e96e663dc3a03c11dea0c4df069278d9c3a
Kay <kwan@wish.com>
2022-02-18 16:27:27 +0800

    show success picture

--- components/upload/doc/base.vue
+++ components/upload/doc/base.vue
@@ -11,7 +11,23 @@ export default {
   data () {
     return {
       multiple: true,
-      fileList: [],
+      fileList: [{
+        name: 'test.jpg',
+        percentage: 100,
+        raw: {
+          uid: 1645171975770,
+          lastModified: 1644907336299,
+          lastModifiedDate: new Date(),
+          name: 'test.jpg',
+          size: 88596,
+          type: 'image/jpeg',
+          webkitRelativePath: '',
+        },
+        size: 88596,
+        status: 'success',
+        uid: 1645171975770,
+        url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F016a0b5e8c681ea8012165183f807a.jpg%403000w_1l_0o_100sh.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647763053&t=57ce7af82e614f2a1373b7f83d338fb5',
+      }],
     };
   },
 };
--- components/upload/doc/picture.vue
+++ components/upload/doc/picture.vue
@@ -7,21 +7,37 @@
       :http-request="uploadPdf"
       :on-preview="handlePictureCardPreview"
       :on-remove="handleRemove"
-      :file-list="fileList">
+      :value="fileList">
       <i class="wticon-add"></i>
     </wt-upload>
-    <wt-modal v-model="dialogVisible">
+    <!-- <wt-modal v-model="dialogVisible">
       <img :src="dialogImageUrl" />
-    </wt-modal>
+    </wt-modal> -->
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      dialogImageUrl: '',
+      dialogImageUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F016a0b5e8c681ea8012165183f807a.jpg%403000w_1l_0o_100sh.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647763053&t=57ce7af82e614f2a1373b7f83d338fb5',
       dialogVisible: false,
-      fileList: [],
+      fileList: [{
+        name: 'test.jpg',
+        percentage: 100,
+        raw: {
+          uid: 1645171975770,
+          lastModified: 1644907336299,
+          lastModifiedDate: new Date(),
+          name: 'test.jpg',
+          size: 88596,
+          type: 'image/jpeg',
+          webkitRelativePath: '',
+        },
+        size: 88596,
+        status: 'success',
+        uid: 1645171975770,
+        url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F016a0b5e8c681ea8012165183f807a.jpg%403000w_1l_0o_100sh.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647763053&t=57ce7af82e614f2a1373b7f83d338fb5',
+      }],
     };
   },
   methods: {
