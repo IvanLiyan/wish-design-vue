b40d17a3887420aedb6a1195790fc746c295954c
Rose <ryu@wish.com>
2022-03-07 16:09:41 +0800

    release: v0.0.2-alpha.13

--- publish/lib/index.js
+++ publish/lib/index.js
@@ -256,7 +256,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-  version: '0.0.2-alpha.12',
+  version: '0.0.2-alpha.13',
   install: install
 });
 
