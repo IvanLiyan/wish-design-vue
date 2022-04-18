cec056dd9a8897df4593e7f502f8748d873b1ddc
Kay <kwan@wish.com>
2022-04-18 17:53:43 +0800

    fix:icon

--- components/icon/custom-icon/check-circle.vue
+++ components/icon/custom-icon/check-circle.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable -->
 
 <template>
-  <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="23" height="22" viewBox="0 0 23 22" xmlns="http://www.w3.org/2000/svg">
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
