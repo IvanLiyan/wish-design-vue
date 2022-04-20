1cb277e65ea83cef3948cfc95174588625c477de
Rose <ryu@wish.com>
2022-04-20 18:28:12 +0800

    add custom icon

--- components/icon/custom-icon/index.js
+++ components/icon/custom-icon/index.js
@@ -1,13 +1,15 @@
 import CheckCircle from './check-circle.vue';
 import Manual from './manual.vue';
 import AssignmentCheck from './assignment-check.vue';
+import FlightTakeoff from './flight-takeoff';
 
-const customIconNames = ['check-circle', 'manual', 'assignment-check'];
+const customIconNames = ['check-circle', 'manual', 'assignment-check', 'flight-takeoff'];
 
 const customIcons = {
   CheckCircle,
   Manual,
   AssignmentCheck,
+  FlightTakeoff,
 };
 
 export default customIcons;
