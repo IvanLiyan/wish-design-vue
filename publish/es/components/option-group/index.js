87a784336af21374ff1c24b4375eff66589c3d2c
Rose <ryu@wish.com>
2022-06-01 20:43:01 +0800

    del publish

--- publish/es/components/option-group/index.js
+++
@@ -1,8 +0,0 @@
-import OptionGroup from './option-group';
-
-/* istanbul ignore next */
-OptionGroup.install = function (Vue) {
-  Vue.component(OptionGroup.name, OptionGroup);
-};
-
-export default OptionGroup;
