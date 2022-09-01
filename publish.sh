48e25fede9906b4b7fd7de077fbce49f54acd890
Namgil Lee <nlee@wish.com>
2022-09-01 16:44:27 -0400

    build on current branch

--- publish.sh
+++ publish.sh
@@ -1,16 +1,16 @@
 # exit when any command fails
 set -e
 
-eval $(ssh-agent -s)
-ssh-add /root/.ssh/id_rsa
-BRANCH_NAME="$(date +%s)"
-git checkout -b $BRANCH_NAME
-git pull --rebase origin master
+#eval $(ssh-agent -s)
+#ssh-add /root/.ssh/id_rsa
+#BRANCH_NAME="$(date +%s)"
+#git checkout -b $BRANCH_NAME
+#git pull --rebase origin master
 
 BUILD_NUMBER="0.0.1-$(date +%s)"
 
+npm install
 npm run build
-cd dist
 
 # Upload javascript package to NPM
 echo "registry=https://npm.infra.wish.com/" > ~/.npmrc
@@ -19,7 +19,7 @@ echo "//npm.infra.wish.com/:_authToken=\"$GITLAB_NPM_TOKEN\"" >> ~/.npmrc
 VERSION_LINE="\"version\": \""$BUILD_NUMBER"\","
 sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' package.json
 sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' $WISH_DESIGN_VUE_HOME/package.json
-npm publish --registry https://npm.infra.wish.com --verbose --scope=@ContextLogic
+npm publish --registry https://npm.infra.wish.com --verbose
 
 #cd $WISH_DESIGN_VUE_HOME
 
