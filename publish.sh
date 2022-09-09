446e70dd9eedb675d7d03e142d23f0629051be76
Namgil Lee <nlee@wish.com>
2022-09-09 17:03:28 -0400

    Removed package-lock.json, Update build command

--- publish.sh
+++ publish.sh
@@ -1,18 +1,18 @@
 # exit when any command fails
 set -e
 
-BUILD_NUMBER="0.0.1-$(date +%s)"
+BUILD_NUMBER="0.0.1-alpha.$(date +%s)"
 
-npm install
+npm run node-i
 npm run build
 
-ls dist
-
 # Upload javascript package to NPM
-#echo "registry=https://npm.infra.wish.com/" > ~/.npmrc
-#echo "//npm.infra.wish.com/:_authToken=\"$GITLAB_NPM_TOKEN\"" >> ~/.npmrc
+echo "registry=https://npm.infra.wish.com/" > ~/.npmrc
+echo "//npm.infra.wish.com/:_authToken=\"$GITLAB_NPM_TOKEN\"" >> ~/.npmrc
 
-#VERSION_LINE="\"version\": \""$BUILD_NUMBER"\","
-#sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' package.json
-#sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' $WISH_DESIGN_VUE_HOME/package.json
-#npm publish --registry https://npm.infra.wish.com --verbose
+VERSION_LINE="\"version\": \""$BUILD_NUMBER"\","
+sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' package.json
+sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' $WISH_DESIGN_VUE_HOME/package.json
+sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' publish/package.json
+sed -i -e 's#"version".*#'"$VERSION_LINE"'#g' $WISH_DESIGN_VUE_HOME/publish/package.json
+npm publish --registry https://npm.infra.wish.com --verbose publish
